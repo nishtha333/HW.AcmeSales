c95d7eeecf8722373919838c16295539a8392ae0
Nishtha Malhotra <nishthamalhotra@gmail.com>
2018-07-07 20:05:21 -0400

    Acme Sales Customer Map HW

---
+++ src/AcmeSales.js
@@ -0,0 +1,21 @@
+module.exports = {
+    //Go through each customer, filter the relevant details from sales and reduce them to a total amount
+    generateCustomerSalesMap: (sales, customers) => {
+        const results = {};
+        customers.forEach((element) => {
+            let filteredSales = sales.filter((s) => s.customerId === element.id);
+            if(!filteredSales.length) {
+                results[element.name] = 0;
+            }
+            else if (filteredSales.length === 1) {
+                results[element.name] = filteredSales[0].total;
+            }
+            else {
+                results[element.name] = filteredSales.reduce((previous, current) => {
+                                                        return previous.total + current.total;
+                                                    }); 
+            }          
+        });
+        return results;
+    }
+}
---
+++ test/AcmeSales.spec.js
@@ -0,0 +1,18 @@
+const expect = require('chai').expect;
+const acmeSales = require('../src/AcmeSales');
+
+describe('Acme Sales', () => {
+    it('exists', () => {
+        expect(acmeSales).to.be.ok;
+    });
+
+    it('generates customer-sales map: tests for customers with no sales, single sale and multiple sales', () => {
+        const sales = [ {customerId: 1, orderId: 1, total: 30}, 
+                        {customerId: 2, orderId: 2, total: 45}, 
+                        {customerId: 1, orderId: 3, total: 25}];
+        const customers = [{id: 1, name: 'moe'}, {id: 2, name: 'larry'}, {id:3, name: 'joe'}];
+        const expectedResult = {moe: 55, larry: 45, joe: 0};
+        expect(acmeSales.generateCustomerSalesMap(sales, customers)).to.eql(expectedResult);    
+    });
+});
+
